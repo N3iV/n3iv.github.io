776ff88a39038ca988d25c63af25a75e468d2359
Hoang Vien <80262438+N3iV@users.noreply.github.com>
2022-07-30 18:15:09 +0700

    add hover effect

--- main.js
+++ main.js
@@ -1,58 +1,15 @@
-window.addEventListener("load", function () {
-  gsap.to(".text-wrapper > div", 1, {
-    x: "500",
-    ease: Expo.easeInOut,
-    delay: 1,
-    stagger: 0.1,
-  });
+//
 
-  gsap.to(".text-wrapper", 3, {
-    y: -600,
-    scale: 4.5,
-    rotate: -90,
-    ease: Expo.easeInOut,
-    delay: 1.5,
-  });
-
-  gsap.to(".text", 1, {
-    opacity: 1,
-    ease: Expo.easeInOut,
-    delay: 3,
-  });
-
-  gsap.to(".text-wrapper > div", 4, {
-    x: "-3500",
-    ease: Expo.easeInOut,
-    delay: 3.5,
-    stagger: 0.05,
-  });
-  gsap.to(".text-wrapper", 3, {
-    opacity: 0,
-    ease: Expo.easeInOut,
-    delay: 4.5,
-  });
-
-  gsap.to(".text-container", 2, {
-    bottom: "-100%",
-    ease: Expo.easeInOut,
-    delay: 6,
-  });
-  gsap.to(".text-wrapper", 4, {
-    display: "none",
-    ease: Expo.easeInOut,
-    delay: 5.5,
-  });
-
-  anime.timeline().add({
-    targets: ".hero ",
-    opacity: [0, 1],
-    translateY: [200, 0],
-    translateZ: 0,
-    easing: "easeOutExpo",
-    duration: 2000,
-    delay: (el, i) => 7000 + 40 * i,
-  });
+// !hover effect
+var hoverDistort = new hoverEffect({
+  parent: document.querySelector(".content__img"),
+  intensity: 0.6,
+  image1: "/assets/me.jpg",
+  image2: "/assets/me_2.jpg",
+  displacementImage: "/assets/8.jpg",
+  imagesRatio: 1.2 / 1,
 });
+// ! end hover effect
 
 let elements = document.querySelectorAll(".nav__link");
 
