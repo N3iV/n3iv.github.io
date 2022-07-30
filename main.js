8cfb0bb67cf8a987b251e701a0d02acffd14a97d
Hoang Vien <80262438+N3iV@users.noreply.github.com>
2022-07-30 16:43:48 +0700

    add animate& fix responsive =))

--- main.js
+++ main.js
@@ -115,3 +115,120 @@ function filterItems(filterBtn) {
   });
   portfolioItems = document.querySelectorAll(".experience__item.show");
 }
+
+const circleArea = document.querySelector(".circle__container__inner");
+let circles = [...document.querySelectorAll(".circle")];
+circles.shift();
+console.log(circles);
+
+let positions = {
+  circleOne: { x: 0, y: 0 },
+  circleTwo: { x: 0, y: 0 },
+  circleThree: { x: 0, y: 0 },
+  circleFour: { x: 0, y: 0 },
+};
+
+let width = window.innerWidth;
+let height = window.innerHeight;
+
+let x = 0;
+let y = 0;
+
+function lerp(start, end, t) {
+  return start * (1 - t) + end * t;
+}
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+const idInterval = setInterval(() => {
+  x = getRndInteger(1000, 1300);
+  y = getRndInteger(100, 700);
+  console.log(x, y);
+}, 400);
+circleArea.addEventListener("mousemove", (e) => {
+  x = e.clientX;
+  y = e.clientY;
+  console.log(x, y);
+  clearInterval(idInterval);
+});
+
+function animate() {
+  // Animate circles
+  positions.circleOne.x = lerp(
+    positions.circleOne.x,
+    (x - width / 2) * 0.2,
+    0.1
+  );
+  positions.circleOne.y = lerp(
+    positions.circleOne.y,
+    (y - height / 2) * 0.2,
+    0.1
+  );
+
+  positions.circleTwo.x = lerp(
+    positions.circleTwo.x,
+    (-x + width / 2) * 0.2,
+    0.1
+  );
+  positions.circleTwo.y = lerp(
+    positions.circleTwo.y,
+    (y - height / 2) * 0.2,
+    0.1
+  );
+
+  positions.circleThree.x = lerp(
+    positions.circleThree.x,
+    (x - width / 2) * 0.2,
+    0.1
+  );
+  positions.circleThree.y = lerp(
+    positions.circleThree.y,
+    (-y + height / 2) * 0.2,
+    0.1
+  );
+
+  positions.circleFour.x = lerp(
+    positions.circleFour.x,
+    (-x + width / 2) * 0.2,
+    0.1
+  );
+  positions.circleFour.y = lerp(
+    positions.circleFour.y,
+    (-y + height / 2) * 0.2,
+    0.1
+  );
+
+  circles[0].style.transform = `translate(-50%, -50%) translate3d(${positions.circleOne.x}px, ${positions.circleOne.y}px, 0)`;
+  circles[1].style.transform = `translate(-50%, -50%) translate3d(${positions.circleTwo.x}px, ${positions.circleTwo.y}px, 0)`;
+  circles[2].style.transform = `translate(-50%, -50%) translate3d(${positions.circleThree.x}px, ${positions.circleThree.y}px, 0)`;
+  circles[3].style.transform = `translate(-50%, -50%) translate3d(${positions.circleFour.x}px, ${positions.circleFour.y}px, 0)`;
+
+  requestAnimationFrame(animate);
+}
+
+animate();
+
+container = document.querySelector(".hero__title");
+
+var text = new Blotter.Text("Hoang Vien", {
+  family: "monospace",
+  bold: 800,
+  size: 100,
+  fill: "#ccd6f6",
+  paddingLeft: 0,
+  paddingRight: 0,
+  paddingTop: 10,
+  paddingBottom: 10,
+});
+
+var material = new Blotter.FliesMaterial();
+material.uniforms.uPointCellWidth.value = 0.02;
+material.uniforms.uPointRadius.value = 2;
+material.uniforms.uSpeed.value = 5;
+
+var blotter = new Blotter(material, {
+  texts: text,
+});
+
+var scope = blotter.forText(text);
+scope.appendTo(container);
